d156179afe112847e34a0e04b15e62773b6c947b
Aleksei <sudenkov.aleksei@mail.ru>
2021-09-25 12:39:00 +0300

    all change

--- src/Utils/Validators.js
+++
@@ -1,9 +0,0 @@
-export const required = (value) => {
-    if(value) return undefined
-              return 'Field is require'
-}
-
-export const maxLenght = (maxLength) => (value) => {
-    if(value.length > maxLength) return `Max length is ${maxLength} simbols`
-                                 return undefined
-}
